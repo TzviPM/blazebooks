e55b73f7752b9c287391a3ba1a837b90afb651ef
Tzvi Melamed <tzvi@melamed.biz>
2024-01-09 17:24:31 -0500

    🎨 Reformat code

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,8 +1,8 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Page from './page'
- 
+import {expect, test} from 'vitest';
+import {render, screen} from '@testing-library/react';
+import Page from './page';
+
 test('Page', () => {
-  render(<Page />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
-})
+  render(<Page />);
+  expect(screen.getByRole('heading', {level: 1, name: 'Home'})).toBeDefined();
+});
